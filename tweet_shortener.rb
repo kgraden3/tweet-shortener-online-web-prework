fa9feb3acf4b8c8d50c0bad13d9f404de53bf256
Kyle Graden <kylegraden3@gmail.com>
2019-03-14 22:48:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,46 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@" ,
+    "and" => "&"
+    }
+end
+
+def word_substituter(tweet)
+  tweet_array = tweet.split(" ");
+  tweet_array.collect do |word|
+    dictionary.collect do |key, value|
+      if key == word
+        word.replace(value)
+      end
+    end
+  end
+  tweet_array.join(" ")
+end
+
+def bulk_tweet_shortener(tweets_array)
+  tweets_array.collect do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+
+def selective_tweet_shortener()
+
+
+
+end
+
+
+def shortened_tweet_truncator()
+
+
+
+end
